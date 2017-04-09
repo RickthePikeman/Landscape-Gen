b59a45179a7a787c4bfd561c40ce94ef8f828245
Josh <Commandercos@hotmail.co.uk>
2017-04-09 14:35:32 +0100

    Nodes have been added that have control nodes which together will be used to from triangular edges of that node

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -35,6 +35,9 @@ public class MapGenerator : MonoBehaviour {
 		{
 			SmoothMap();
 		}
+
+		MeshGenerator meshGen = GetComponent<MeshGenerator>();
+		meshGen.GenerateMesh(map, 1);
     }
 
     void RandomFillMap()
@@ -101,18 +104,18 @@ public class MapGenerator : MonoBehaviour {
 
     void OnDrawGizmos()
     {
-        if(map != null)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-					Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
-					Vector3 pos = new Vector3(-width / 2 + x + 0.5f, -height / 2 + y + 0.5f);
-					Gizmos.DrawCube(pos, Vector3.one);
-                }
-            }
-        }
+     //   if(map != null)
+     //   {
+     //       for (int x = 0; x < width; x++)
+     //       {
+     //           for (int y = 0; y < height; y++)
+     //           {
+					//Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+					//Vector3 pos = new Vector3(-width / 2 + x + 0.5f, -height / 2 + y + 0.5f);
+					//Gizmos.DrawCube(pos, Vector3.one);
+     //           }
+     //       }
+     //   }
     }
 
 }
