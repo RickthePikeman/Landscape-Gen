fa86d727d9f7821872c0da63b7026dc3c96f1e7c
RickthePikeman <Commandercos@hotmail.co.uk>
2017-07-19 21:16:23 +0100

    Made an isometric camera and a basic map generator using existing prefabs

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int width, height;
+    public GameObject Tile;
+
+    int[,] map;
+    public string seed;
+
+	// Use this for initialization
+	void Start () {
+        GenerateMap();
+
+    }
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+
+    void GenerateMap()
+    {
+        Debug.Log("Doing it");
+        map = new int[width, height];
+        randomiseElements();
+
+        int borderSize = 5;
+        int[,] borderedMap = new int[width + borderSize * 2, height + borderSize * 2];
+
+        for (int x = 0; x < borderedMap.GetLength(0); x++)
+        {
+            for (int y = 0; y < borderedMap.GetLength(1); y++)
+            {
+                if (x >= borderSize && x < width + borderSize && y >= borderSize && y < height + borderSize)
+                {
+                    borderedMap[x, y] = map[x - borderSize, y - borderSize];
+                }
+                else
+                {
+                    borderedMap[x, y] = 1;
+                }
+            }
+        }
+
+        //Instantiate(Resources.Load("Tile"),)
+        
+        
+        //public 
+    }
+
+    void randomiseElements()
+    {
+        
+        System.Random pseudoRandom = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                {
+                    map[x, y] = 0;
+                }
+                else
+                {
+                    map[x, y] = 1;
+                }
+                Vector3 pos = new Vector3(x, 0, y) * 1;
+                Instantiate(Tile, pos, Quaternion.identity);
+            }
+        }
+    }
+
+}
+
---
+++ Assets/TerrainEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainEvents : MonoBehaviour {
+
+    public Material[] materials;
+    //public Renderer rend;
+    public MeshRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<MeshRenderer>();
+        rend.enabled = true;
+        //materials = rend.GetComponents<Material>();
+        //rend.sharedMaterial = materials[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseUp()
+    {
+        //Debug.Log(materials.Length);
+        rend.sharedMaterial = materials[1];
+
+    }
+}
